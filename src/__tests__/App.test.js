0dd2ee84fec2d418523363d4522eca5f214bfcd7
wassermandh <daniel_w@getbento.com>
2021-06-11 12:54:40 -0400

    test cov testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import {add} from '../App'
+var assert = require('assert');
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('add shouold work', () => {
+  const result = add(1,2)
+  assert(result === 3)
+})
