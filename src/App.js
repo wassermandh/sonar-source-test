5010d02bd5db45d68cd0c558173d81a402d82b63
wassermandh <daniel_w@getbento.com>
2021-06-11 12:39:03 -0400

    test cov

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ export function add(num1, num2) {
 }
 
 function App() {
+  const result = add(1,2)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>{result} and save to reload.
         </p>
         <a
           className="App-link"
